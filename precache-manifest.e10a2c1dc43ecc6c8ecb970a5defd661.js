3cc71e81b5bd8dd701466f121053d6385cec2d17
shubhammapara <shubham.mapara@numerator.com>
2022-06-12 10:39:43 +0530

    Updates

---
+++ precache-manifest.e10a2c1dc43ecc6c8ecb970a5defd661.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d96da9b99d123d1e153db11f9554e12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c792c2cf4066f484622b",
+    "url": "/static/css/2.9c389199.chunk.css"
+  },
+  {
+    "revision": "9c723820f4495e8d3c2f",
+    "url": "/static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "c792c2cf4066f484622b",
+    "url": "/static/js/2.5afce833.chunk.js"
+  },
+  {
+    "revision": "0297ac78c823ef9b172e38637e06e92e",
+    "url": "/static/js/2.5afce833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c723820f4495e8d3c2f",
+    "url": "/static/js/main.eb897a2a.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
